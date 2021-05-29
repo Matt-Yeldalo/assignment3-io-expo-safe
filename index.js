0297e3051d0511f00420f26006e5cb44d88bb5a4
Matt <matman370@hotmail.com>
2021-05-30 01:08:26 +1000

    removed fontloader from index

--- index.js
+++ index.js
@@ -5,10 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App.js';
 import {name as appName} from './app.json';
-import WebFont from 'webfontloader';
-// WebFont.load({
-//   google: {
-//     families: ['Lobster', 'Cairo', 'Fira Sans']
-//   },
-// });
+
 AppRegistry.registerComponent(appName, () => App);
